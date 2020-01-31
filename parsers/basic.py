004106d3df5bf9110a5f0ecfd63ecd6c68baf6c0
roganovich <roman.roganovich@mail.ru>
2020-01-31 10:36:35 +0300

    Focusauto

--- parsers/basic.py
+++ parsers/basic.py
@@ -220,7 +220,7 @@ class Basic:
             for row in reader:
                 i = i + 1
                 # пропускаем первую строку
-                if (self.firstLine == True and i == 1):
+                if (self.clearLine  and i <= self.firstLine):
                     continue
                 # берем из строки только нужные столбцы
                 colData = self.prepareColumns(row)
