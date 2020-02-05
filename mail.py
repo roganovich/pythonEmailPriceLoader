c8d9b631d69aa880edf598ad3c26dc37fb1bf267
roganovich <roman.roganovich@mail.ru>
2020-02-05 16:49:34 +0300

    add shatem parser

--- mail.py
+++ mail.py
@@ -42,6 +42,7 @@ class MailLoader():
         except:
             # журнал
             log.print_r("Не удалось подключиться к " + username)
+            exit()
 
     # функция проверяет строку на кодировку base64(ей кодируют кирилицу)
     def hascyrillic(self, s):
