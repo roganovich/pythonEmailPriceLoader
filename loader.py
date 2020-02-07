53ca93a68bde85095399f160b3110ec2d0349cf0
roganovich <roman.roganovich@mail.ru>
2020-02-07 12:20:06 +0300

    add sum strip to string

--- loader.py
+++ loader.py
@@ -53,8 +53,8 @@ class Loader:
 		prfc_prices_file_id = self.prf_id
 		prfc_brand = str(re.sub(r'[^0-9A-Za-zа-яА-ЯёЁ\s+]+', r'', data[0].strip()))
 		prfc_article = str(re.sub(r'[^0-9A-Za-z\s+]+', r'', data[1].strip()))
-		prfc_price = re.sub(r'[^0-9.]+', r'', data[2].strip().replace(',', '.'))
-		prfc_quality = round(re.sub(r'[^0-9.]+', r'', data[3].strip().replace(',', '.')))
+		prfc_price = float(re.sub(r'[^0-9.]+', r'', data[2].strip().replace(',', '.')))
+		prfc_quality = round(float(re.sub(r'[^0-9.]+', r'', data[3].strip().replace(',', '.'))))
 
 		query = ("INSERT INTO public.prices_file_col(prfc_prices_file_id, prfc_brand,  prfc_article, prfc_price, prfc_quality) VALUES (%s, %s, %s, %s)")
 		data = (prfc_prices_file_id, prfc_brand, prfc_article, prfc_price, prfc_quality)
