ab3ac14f692db58cb23fb821647f9b4aec22abb0
roganovich <roman.roganovich@mail.ru>
2020-02-08 11:32:32 +0300

    08-02-2020

--- loader.py
+++ loader.py
@@ -74,7 +74,7 @@ class Loader:
 	def writerests(self, data):
 		log.print_r(data)
 		query = ("INSERT INTO public.prices_file_col(prfc_prices_file_id, prfc_brand,  prfc_article, prfc_price, prfc_quality) VALUES (%s, %s, %s, %s, %s)")
-		dataClear = (data.prfc_prices_file_id, data.prfc_brand, data.prfc_article, data.prfc_price, data.prfc_quality)
+		dataClear = (data['prfc_prices_file_id'], data['prfc_brand'], data['prfc_article'], data['prfc_price'], data['prfc_quality'])
 		log.print_r(dataClear)
 		self.cursor.execute(query, dataClear)
 		self.conn.commit()
