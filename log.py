d03b243d2e7a2461f7dfbabbc67327d66ec9cf82
roganovich <roman.roganovich@mail.ru>
2020-02-28 09:22:28 +0300

    fix log text

--- log.py
+++ log.py
@@ -1,8 +1,10 @@
 # author Roganovich.R.M.
 import config
+import datetime
 # получаем настройки приложения
 config = config.getConfig()
 
 def print_r(str):
     if (config.get("system", "print") == "1"):
-        print(str)
+        today = datetime.datetime.today()
+        print(today.strftime("%Y-%m-%d %H:%M:%S") + " " + str)
