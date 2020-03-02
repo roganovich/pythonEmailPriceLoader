c4b1f79d2e4f47c5bda01bb87fb59c5f865b329f
roganovich <roman.roganovich@mail.ru>
2020-03-02 10:23:00 +0300

    add Avtosputnik

--- parsers/avtosputnik.py
+++ parsers/avtosputnik.py
@@ -18,7 +18,7 @@ class Avtosputnik(Basic):
 	# тип файла вложения
 	parsertype = "zip"
 	# тип файла прайса
-	filetype = "xlsx"
+	filetype = "xls"
 
 	# сопостовляем колонки в файле с назначениями полей
 	colums = {"art": 0, "bra": 1, "price": 3, "quality": 4, "desc":2, "art_sup":0}
