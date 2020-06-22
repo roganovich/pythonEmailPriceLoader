de3269f1a4820806e742332831d939f9f6c3a202
Roman Roganovich (Soft) <roman.roganovich@mail.ru>
2020-06-22 11:49:20 +0300

    add parthouse

--- parsers/parthouse.py
+++ parsers/parthouse.py
@@ -21,6 +21,7 @@ class PartHouse(Basic):
 	parsertype = "zip"
 	# тип файла прайса
 	filetype = "csv"
+	fileEncoding = "utf-8"
 	# сопостовляем колонки в файле с назначениями полей
 	colums = {"art": 0, "bra": 1, "price": 4, "quality": 3, "desc":2, "art_sup":0}
 
