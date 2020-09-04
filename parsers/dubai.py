993d38695ce3c8bc5794656342e671024a04b0fb
Roman Roganovich (Soft) <roman.roganovich@mail.ru>
2020-09-04 10:53:44 +0300

    add dubai file

--- parsers/dubai.py
+++ parsers/dubai.py
@@ -20,7 +20,7 @@ class Dubai(Basic):
 	filePathExtract = "files/Dubai/"
 	# тип файла прайса
 	filetype = "csv"
-	parsertype = "file"
+	parsertype = "zip"
 	fileEncoding = "utf-8"
 	# делитель CSV
 	delimiter = "\t"
