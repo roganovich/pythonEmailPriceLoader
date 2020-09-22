5f76567ca745d6ac6ab6dd8110add787775852a4
Roman Roganovich (Soft) <roman.roganovich@mail.ru>
2020-09-22 12:08:31 +0300

    fix price tavria

--- parsers/tavriyaauto.py
+++ parsers/tavriyaauto.py
@@ -19,13 +19,13 @@ class Tavriyaauto(Basic):
 	# тип файла вложения
 	parsertype = "file"
 	# тип файла прайса
-	filetype = "xlsx"
+	filetype = "xls"
 	fileEncoding = "utf-8"
-	bra_name = 'MERCEDES-BENZ'
-	bra_id = 553
+	#bra_name = 'MERCEDES-BENZ'
+	#bra_id = 553
 
 	# сопостовляем колонки в файле с назначениями полей
-	colums = {"art": 0, "bra": 0, "price": 3, "quality": 2, "desc":1, "art_sup":0}
+	colums = {"art": 1, "bra": 3, "price": 5, "quality": 4, "desc":0, "art_sup":1}
 
 
 
